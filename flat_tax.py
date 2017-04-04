018c5de0b4d45466d29b277fcdb077cc99189621
GrossmanJoshua <joshua.j.grossman@me.com>
2017-04-04 15:37:15 -0400

    Update flat_tax.py

--- flat_tax.py
+++ flat_tax.py
@@ -14,14 +14,21 @@ def taxamount(inc):
      tot += (inc-tb[idx])*rate[idx]
      return tot
 
-def compute_rate(current_income_tax_income, total_income, npeople, equalrate_dollars, equalrate_tax):
-     return (current_income_tax_income - npeople*equalrate_tax) / (total_income - npeople * equalrate_dollars)
+def compute_rate(current_income_tax_income, total_income, npeople, offset):
+      b = -total_income
+      a = npeople * offset
+      c = current_income_tax_income
+      R = (-b - np.sqrt(b*b - 4*a*c))/(2 * a)
+      return R,offset
 
-i = 191650
-R = compute_rate(2.6e12, 15e12, 200e6, i, taxamount(i))
-C = i-taxamount(i)/R
+# http://www.usgovernmentrevenue.com/current_revenue
+R,C = compute_rate(2.6e12, 15e12, 200e6, i, 25000)
 
 incxs = np.logspace(3,np.log10(5000000),1000)
 amnts = A([taxamount(i) for i in incxs])
+
+# https://www.cbo.gov/sites/default/files/114th-congress-2015-2016/reports/51361-FigureData.xlsx
+x = 1000.*A([8.3,32.6,58.6,94.9,140.3,195.5,259.9,321.5,1570.8]); y = A([3.3,8.4,12.8,17.0,20.7,23.0,26.3,26.3,34.0])/100.
+
 
 clf(); plot(incxs,amnts); plot(incxs, np.clip(np.polyval([R,-C*R],incxs), 0, float('inf'))); xlim(0,500e3), ylim(0,200e3)
